2fc99a39f36fe1ef0fb7bc7522114c9d01b7a596
askillofgod159 <askillofgod159@naver.com>
2020-09-13 21:26:58 +0900

    09.13 commit

---
+++ src/LifeCoding/Ex01.java
@@ -0,0 +1,18 @@
+package LifeCoding;
+
+public class Ex01 {
+
+	public static void main(String[] args) {
+		
+		for(int i = 3; i<=8 ; i++) {
+			if(i == 3 || i == 5) {
+				System.out.println("["+i+"]´Ü");
+			}
+			for(int k=1 ; k<=9 ; k++) {
+				System.out.println(i+"*"+k +"=" + i*k);
+			}
+		}
+
+	}
+
+}
